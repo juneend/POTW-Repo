3aa458ab87a37f241e9a8bd7da57175dd77e00e0
juneend <juneendstrasser@gmail.com>
2026-07-17 13:17:07 -0700

    finalized agent pathfinding, created prefabs for agent and navmesh

--- Assets/Unity Scripts/Pathfinding/Agent.cs
+++ Assets/Unity Scripts/Pathfinding/Agent.cs
@@ -6,11 +6,15 @@ public class Agent : MonoBehaviour
     public Transform target;
 
     NavMeshAgent agentComp;
+    Animator anim;
+    Vector2 Oldmovement;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         agentComp = GetComponent<NavMeshAgent>();
+        anim = GetComponent<Animator>();
+
         agentComp.updateRotation = false;
         agentComp.updateUpAxis = false;
     }
@@ -18,8 +22,33 @@ public class Agent : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //set the new agent destination
         agentComp.SetDestination(target.position);
         
+        //lock 2d rotation
         transform.rotation = Quaternion.identity;
+
+        //read the agent's movement vector
+        Vector2 movement = agentComp.desiredVelocity.normalized;
+
+        //is the agent moving?
+        if (movement.sqrMagnitude >= 0.01f)
+        {
+
+            Oldmovement = movement;
+
+            anim.SetBool("isWalking", true);
+
+            anim.SetFloat("InputX", movement.x);
+            anim.SetFloat("InputY", movement.y);
+        }
+        else
+        {
+            anim.SetBool("isWalking", false);
+
+            anim.SetFloat("LastInputX", Oldmovement.x);
+            anim.SetFloat("LastInputY", Oldmovement.y);
+        }
+
     }
 }
