4dc62fc92afcbafbbfe275dd27d64a9174fc0241
Nicole-Arii <nicole.lent@outlook.com>
2026-07-21 15:24:46 -0700

    editing loadf level and office

--- Assets/Unity Scripts/Actions/LoadLevelAction.cs
+++ Assets/Unity Scripts/Actions/LoadLevelAction.cs
@@ -8,10 +8,18 @@ public class LoadLevelAction : Action
 	public string levelName = SAME_SCENE;
 
 	public const string SAME_SCENE = "0";
-	
+
+	/// <summary>
+	/// public parameterless method that can be called directly by unityevents
+	/// </summary>
 
 	//Loads a new Unity scene, or reload the current one (it means all objects are reset)
-	public override bool ExecuteAction(GameObject dataObject)
+	public void LoadLevel() {
+		ExecuteAction(null);
+	}
+    //Loads a new Unity scene, or reload the current one (it means all objects are reset)
+
+    public override bool ExecuteAction(GameObject dataObject)
 	{
 		if(levelName == SAME_SCENE)
 		{
