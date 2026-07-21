be4f9b88c1f0e2c897fee41f36496f2e834394d3
rachellent01-stack <rachellent01@gmail.com>
2026-07-21 13:29:28 -0700

    starting news puzzl

--- Assets/PuzzleSlot.cs
+++ Assets/PuzzleSlot.cs
@@ -1,6 +1,7 @@
 using UnityEngine;
 using System.Collections;
 using System.Collections.Generic;
+using UnityEngine.SceneManagement;
 
 
 public class PuzzleSlot : MonoBehaviour
@@ -9,6 +10,26 @@ public class PuzzleSlot : MonoBehaviour
 
     [SerializeField] private AudioSource _source;
 
+    public Sprite lettuceSprite;
+    public Sprite newspaperSprite;
+
+    public void Start()
+    {
+
+        Scene scene = SceneManager.GetActiveScene();
+
+
+
+        _renderer = GetComponent<SpriteRenderer>();
+        if (scene.name == "Newspaper_pzl")
+        {
+            _renderer.sprite = newspaperSprite;
+        }
+        else if (scene.name == "Lettuce_pzl")
+        {
+            _renderer.sprite = lettuceSprite;
+        }
+    }
 
     public void Placed()
     {
